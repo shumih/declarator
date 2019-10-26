c1477a064cfa698d4f64757fe3512f3e669d6adb
Anton Shumikhin <shumih.anton@gmail.com>
2019-10-26 23:53:07 +0300

    kal

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,68 @@
 <script>
-	export let name;
+	import { Router, Route } from 'svelte-routing';
+	import NavBar from './components/NavBar.svelte';
+	import Politicians from './pages/Politicians.svelte';
+	import Sidebar from "./components/Sidebar.svelte";
+
+	export let url = 'politicians';
 </script>
 
 <style>
-	h1 {
-		color: purple;
+	:global(html, body) {
+		height: 100%;
+		width: 100%;
 	}
-</style>
 
-<h1>Hello {name}!</h1>
+	:global(body) {
+		padding: 0;
+		margin: 0;
+
+		--text-black: #313131;
+		--text-grey: #A9A9A9;
+		--text-header-size: 16px;
+	}
+
+	:global(body *) {
+		box-sizing: border-box;
+	}
+	.app {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		grid-template-columns: 200px 1fr;
+		grid-template-rows: 64px 1fr;
+		grid-template-areas:
+			"sidebar header"
+			"sidebar content"
+			"sidebar content";
+	}
+	.header {
+		grid-area: header;
+		border-bottom: 1px solid var(--text-grey);
+	}
+	.sidebar {
+		grid-area: sidebar;
+		box-shadow: 0 0 17px rgba(100, 100, 100, 0.12)
+	}
+	.content {
+		grid-area: content;
+	}
+
+
+</style>
+<div class="app">
+	<Router {url} >
+		<header class="header">
+			<NavBar />
+		</header>
+		<div class="sidebar">
+			<Sidebar />
+		</div>
+		<div class="content">
+			<Route path="politicians" component={Politicians} />
+			<Route path="*" component={Politicians} />
+		</div>
+	</Router>
+</div>
+
+
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,67 @@
+<script>
+    const sidebarItems = [
+        'Президент РФ',
+        'Администрация Президента РФ',
+        'Правительство РФ',
+        'Министерства РФ',
+        'Федеральные службы и агентства',
+        'Совет Федерации',
+        'Госдума',
+        'Конституционный суд'
+    ]
+</script>
+
+<style>
+    .sidebar {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .sidebar-item {
+        display: flex;
+        padding: 8px;
+    }
+    .sidebar-item-icon {
+        display: block;
+        margin: 0 8px;
+    }
+    .sidebar-item-text {
+        font-family: Helvetica CE, sans-serif;
+        font-size: 12px;
+        line-height: 16px;
+        color: var(--text-grey);
+    }
+    .sidebar-item-text:hover {
+        cursor: pointer;
+        color: var(--text-black);
+    }
+
+    .logo {
+        height: 24px;
+        display: flex;
+        align-items: center;
+        padding: 30px 20px;
+    }
+
+    .logo-text {
+        margin-left: 12px;
+        font-family: Helvetica CE, sans-serif;
+        font-size: 14px;
+        line-height: 18px;
+        font-weight: bold;
+        color: var(--text-black);
+    }
+</style>
+
+<div class="logo">
+    <img src="/assets/logo.svg" />
+    <span class="logo-text">Declarator</span>
+</div>
+<div class="sidebar">
+    {#each sidebarItems as sidebarItem}
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="/assets/sidebar-item-icon.svg" />
+            <span class="sidebar-item-text">{sidebarItem}</span>
+        </div>
+    {/each}
+</div>
